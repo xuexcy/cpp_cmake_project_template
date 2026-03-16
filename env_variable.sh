a881b3aa00630cc8921984718cd72d2465ed8ee2
xuexcy <xuechengyun@gmail.com>
2026-03-16 22:48:38 +0800

    add cpp modules

--- env_variable.sh
+++ env_variable.sh
@@ -8,10 +8,23 @@ fi
 
 echo "Project root dir:" $this_script_dir
 echo ''
+export cpu_core_num=`getconf _NPROCESSORS_ONLN`
 
-export compiler=clang++
-#export compiler=g++
+
+
+
+#export compiler=clang++
+export compiler=g++
+export use_cpp_modules=1 # 只在 ubuntu 上测试通过了, macos 上还不行
 export default_execute_file=virtual_friend_function
+#export default_execute_file=cpp23_module_main
+
+
+
+
+if [[ ${use_cpp_modules} -eq 1 ]]; then
+    echo "Build cpp modules with ninja"
+fi
 
 # cmake 工作目录
 if [[ $compiler == "g++" ]];
